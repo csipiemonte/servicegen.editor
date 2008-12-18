16ff6b9ef8504a33dbe4eb5c300e68435c78dfad
ANTONACI Mauro 869 <mauro.antonaci@csi.it>
2008-12-18 10:25:55 +0000

    primo inserimento in SVN + modifica del wizard di new servicegen model: aggiunta automatica dei base types
    git-svn-id: http://subversion.csi.it:10090/workarea/mddtools/servicegen.editor/trunk@51 be04b910-fd5c-0410-891f-bca81b977169

---
+++ src/it/csi/mddtools/servicegen/presentation/Servicegen_metamodelEditorPlugin.java
@@ -0,0 +1,104 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package it.csi.mddtools.servicegen.presentation;
+
+import it.csi.mddtools.appresources.provider.Resources_metamodelEditPlugin;
+
+import it.csi.mddtools.servicedef.provider.Servicedef_metamodelEditPlugin;
+
+import it.csi.mddtools.typedef.provider.Typedef_metamodelEditPlugin;
+
+import org.eclipse.emf.common.EMFPlugin;
+
+import org.eclipse.emf.common.ui.EclipseUIPlugin;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+/**
+ * This is the central singleton for the Servicegen_metamodel editor plugin.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public final class Servicegen_metamodelEditorPlugin extends EMFPlugin {
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final Servicegen_metamodelEditorPlugin INSTANCE = new Servicegen_metamodelEditorPlugin();
+	
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static Implementation plugin;
+
+	/**
+	 * Create the instance.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Servicegen_metamodelEditorPlugin() {
+		super
+			(new ResourceLocator [] {
+				Resources_metamodelEditPlugin.INSTANCE,
+				Servicedef_metamodelEditPlugin.INSTANCE,
+				Typedef_metamodelEditPlugin.INSTANCE,
+			});
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
+	}
+	
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
+	
+	/**
+	 * The actual implementation of the Eclipse <b>Plugin</b>.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static class Implementation extends EclipseUIPlugin {
+		/**
+		 * Creates an instance.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @generated
+		 */
+		public Implementation() {
+			super();
+	
+			// Remember the static instance.
+			//
+			plugin = this;
+		}
+	}
+
+}
