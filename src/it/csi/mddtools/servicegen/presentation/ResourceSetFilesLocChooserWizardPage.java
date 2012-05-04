e97ebb9616573170f69ddaff378986cc6c831e0d
SABATINO Valentina 72022 <valentina.sabatino@consulenti.csi.it>
2012-05-04 09:55:59 +0000

    git-svn-id: http://subversion.csi.it:10090/workarea/mddtools/servicegen.editor/trunk@4477 be04b910-fd5c-0410-891f-bca81b977169

--- src/it/csi/mddtools/servicegen/presentation/ResourceSetFilesLocChooserWizardPage.java
+++ src/it/csi/mddtools/servicegen/presentation/ResourceSetFilesLocChooserWizardPage.java
@@ -34,7 +34,6 @@ import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
 import org.eclipse.jface.dialogs.IDialogPage;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
