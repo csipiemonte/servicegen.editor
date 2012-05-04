7146019b65ead034dc087aa9280d6c562159d444
SABATINO Valentina 72022 <valentina.sabatino@consulenti.csi.it>
2012-05-04 09:46:26 +0000

    git-svn-id: http://subversion.csi.it:10090/workarea/mddtools/servicegen.editor/trunk@4475 be04b910-fd5c-0410-891f-bca81b977169

--- src/it/csi/mddtools/appresources/presentation/AppresourcesModelWizard.java
+++ src/it/csi/mddtools/appresources/presentation/AppresourcesModelWizard.java
@@ -600,7 +600,7 @@ public class AppresourcesModelWizard extends Wizard implements INewWizard {
 	 * The framework calls this to create the contents of the wizard.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 		@Override
 	public void addPages() {
