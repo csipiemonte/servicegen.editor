b6be7126c52d05f0c31719a9109037283d01aeb3
ANTONACI Mauro 869 <mauro.antonaci@csi.it>
2015-11-30 09:00:11 +0000

    STDMDD-1722: modificata documentazione RCAnnotationDetail e rigenerato model/edit/editor
    git-svn-id: http://subversion.csi.it:10090/workarea/mddtools/servicegen.editor/trunk@6414 be04b910-fd5c-0410-891f-bca81b977169

--- src/it/csi/mddtools/servicedef/presentation/ServicedefEditor.java
+++ src/it/csi/mddtools/servicedef/presentation/ServicedefEditor.java
@@ -971,7 +971,7 @@ public class ServicedefEditor
 	 * @generated
 	 */
 	public void createModel() {
-		URI resourceURI = EditUIUtil.getURI(getEditorInput());
+		URI resourceURI = EditUIUtil.getURI(getEditorInput(), editingDomain.getResourceSet().getURIConverter());
 		Exception exception = null;
 		Resource resource = null;
 		try {
@@ -999,10 +999,11 @@ public class ServicedefEditor
 	 * @generated
 	 */
 	public Diagnostic analyzeResourceProblems(Resource resource, Exception exception) {
-		if (!resource.getErrors().isEmpty() || !resource.getWarnings().isEmpty()) {
+		boolean hasErrors = !resource.getErrors().isEmpty();
+		if (hasErrors || !resource.getWarnings().isEmpty()) {
 			BasicDiagnostic basicDiagnostic =
 				new BasicDiagnostic
-					(Diagnostic.ERROR,
+					(hasErrors ? Diagnostic.ERROR : Diagnostic.WARNING,
 					 "servicegen.editor",
 					 0,
 					 getString("_UI_CreateModelError_message", resource.getURI()),
